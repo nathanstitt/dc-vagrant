abc41588f787e81367ed7ca2c46c0568640d269c
Nathan Stitt <nathan@stitt.org>
2013-05-01 16:46:00 -0500

    Default user to 'vagrant'
    This way there won't be multiple accounts setup

--- cookbooks/documentcloud/attributes/default.rb
+++ cookbooks/documentcloud/attributes/default.rb
@@ -1,5 +1,5 @@
-default[:account][:login]  = 'dcloud'
-default[:account][:ssh_keys]=[]
+default[:account][:login]  = 'vagrant'
+default[:account][:ssh_keys]=['ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key']
 
 default[:documentcloud][:directory]  = "/home/#{node[:account][:login]}/documentcloud"
 default[:documentcloud][:rails_env]  = 'development'
@@ -40,4 +40,3 @@ node['postgresql']['pg_hba'] = [
   {:type => 'host', :db => 'all', :user => 'all', :addr => '127.0.0.1/32', :method => 'md5'},
   {:type => 'host', :db => 'all', :user => 'all', :addr => '::1/128', :method => 'md5'}
 ]
-
